7d7e11530486810e2ec7638f51e8140fa4da13ce
olivena <olivia.takkinen@gmail.com>
2025-03-28 15:31:10 +0200

    initialized chakra UI, cleaned the files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to White Lotus forum
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/tuo-mikko/secure-react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git repository
         </a>
       </header>
     </div>
