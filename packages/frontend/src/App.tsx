8765a6e482503296dc2d9a1ff8fba852caf15476
Mikko Tuominen <mikko.i.tuominen@gmail.com>
2025-03-31 15:30:55 +0300

    Changed from threads to posts

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to White Lotus forum
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/tuo-mikko/secure-react-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Git repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
