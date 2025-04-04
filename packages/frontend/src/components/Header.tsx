1f6873c1880debb19736fa7f467c3b137c359157
olivena <olivia.takkinen@gmail.com>
2025-04-04 13:09:50 +0300

    forum shows database material

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Flex, Text, Button } from '@chakra-ui/react';
+//import { Link } from 'react-router-dom';
+import { Flex, Text, Button } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 
--- packages/frontend/src/pages/Forum.tsx
+++ packages/frontend/src/pages/Forum.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Flex, Text } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import axios from 'axios';
 
-function Forum() {
+const baseUrl = 'http://localhost:3001/api/posts'
+
+const Forum = () => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const fetchPosts = () => {
+        axios.get(baseUrl)
+        .then(response => {
+            console.log(response.data);
+            setPosts(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        }); 
+    };
 
     return (
-        <Flex direction='column' align="center">
+        <Flex 
+            direction='column' 
+            align="center"
+            gap="4">
             <Text
                 fontSize="5xl"
                 fontWeight="bold"
@@ -12,6 +33,33 @@ function Forum() {
             >
                 FORUM
             </Text>
+            <Flex>
+                <Button
+                    onClick={() => console.log("new post")}
+                    >
+                    New Post
+                </Button>
+            </Flex>
+            <Flex
+                bg="#f4f1bb"
+                p="6"
+                borderRadius="md"
+                gap="1"
+                direction = "column"
+                >
+                {posts.map((post) => (
+                    <Flex
+                        bg="white"
+                        p="5"
+                        borderRadius="md"
+                        borderWidth="1"
+                        
+                        >
+                        <h3>{post.title}</h3>
+                        <h4>{post.message}</h4>
+                    </Flex>
+                ))}
+            </Flex>
         </Flex>
     )
 }
