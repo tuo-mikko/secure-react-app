97e07bf1aadc38fb86897e06a1e338da447acbdc
olivena <olivia.takkinen@gmail.com>
2025-04-22 15:52:41 +0300

    added recent threads to home page

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Flex, 
+    Text,
+    Grid, 
+    Heading,
+    GridItem } from '@chakra-ui/react'
 
-import { Flex, Text } from '@chakra-ui/react'
+const baseUrl = 'http://localhost:3001/api/posts?limit=2&sort=desc'
+interface Post {
+    title: string;
+    message: string;
+};
 
 function Home() {
+    const [posts, setPosts] = useState([]); //db posts
+    useEffect(() => {
+            fetchPosts();
+        }, []);
+
+    //fetch forumposts from database
+    const fetchPosts = () => {
+        axios.get(baseUrl)
+        .then(response => {
+            console.log(response.data);
+            setPosts(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        }); 
+    };
     return (
         <Flex direction='column' align="center">
             <Text
@@ -12,7 +38,40 @@ function Home() {
             >
                 Welcome to White Lotus -resort
             </Text>
-            <Text>moi</Text>
+            <Grid
+                h="200px"
+                templateRows="repeat(3, 1fr)"
+                templateColumns="repeat(3,1fr)"
+            >
+                    <GridItem rowSpan={3} colSpan={2}>
+                    <Text>Recent threads</Text>
+                    <Flex
+                        bg="#f4f1bb"
+                        p="6"
+                        borderRadius="md"
+                        gap="1"
+                        direction = "column"
+                        borderWidth="1"
+                        borderColor="#37371f"
+                        width="30"
+                    >
+                        {posts
+                        .slice(0,4)
+                        .map((post) => (
+                            <Flex
+                                bg="white"
+                                p="5"
+                                borderRadius="md"
+                                borderWidth="1"
+                                direction = "column"
+                                >
+                                <Heading>{post.title}</Heading>
+                                <p>{post.message}</p>
+                            </Flex>
+                        ))}
+                    </Flex>
+                </GridItem>
+            </Grid>
         </Flex>
     )
 }
