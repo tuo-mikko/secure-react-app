aeb9045f67c3f4dfeb6923aad6a6486358c5a1e9
olivena <olivia.takkinen@gmail.com>
2025-04-23 11:06:26 +0300

    added login form to home page

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Flex, 
+import { PasswordInput } from "../components/ui/password-input"
+import loginService from '../services/login'
+import { 
+    Box,
+    Button,
+    Flex, 
+    Field,
     Text,
     Grid, 
     Heading,
-    GridItem } from '@chakra-ui/react'
+    GridItem,
+    Stack,
+    Input 
+} from '@chakra-ui/react'
 
 const baseUrl = 'http://localhost:3001/api/posts'
 interface Post {
@@ -15,6 +24,11 @@ interface Post {
 
 function Home() {
     const [post, setPost] = useState<Post[]>([]); //db posts
+    const [username, setUsername] = React.useState('');
+    const [user, setUser] = React.useState(null);
+    const [password, setPassword] = React.useState('');
+    const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+
     useEffect(() => {
             fetchPosts();
         }, []);
@@ -30,6 +44,27 @@ function Home() {
             console.error('Error fetching data:', error);
         }); 
     };
+
+    const handleLogin = async (event: React.FormEvent<HTMLElement>) => {
+        event.preventDefault()    
+        console.log('logging in with', username, password)  
+
+        try {
+            const user = await loginService.login({
+                username, password,
+            });
+
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
     return (
         <Flex direction='column' align="center">
             <Text
@@ -41,10 +76,10 @@ function Home() {
             </Text>
             <Grid
                 h="200px"
-                templateRows="repeat(3, 1fr)"
+                templateRows="repeat(5, 1fr)"
                 templateColumns="repeat(3,1fr)"
             >
-                    <GridItem rowSpan={3} colSpan={2}>
+                <GridItem rowSpan={3} colSpan={2} padding="4">
                     <Text fontSize="2xl">Recent threads</Text>
                     <Flex
                         bg="#f4f1bb"
@@ -72,6 +107,42 @@ function Home() {
                             </Flex>
                         ))}
                     </Flex>
+                </GridItem>
+                <GridItem rowSpan={2} colSpan={1} padding="4">
+                    <Box
+                        p="6"
+                        gap="1"
+                        borderWidth="5"
+                        borderRadius="md"
+                        borderColor="#37371f"
+                    >
+                        <Text>Login with user account</Text>
+                        <form onSubmit={handleLogin}>
+                            <Stack gap="4" align="flex-start" maxW="sm">
+                                <Field.Root>
+                                    <Field.Label>Username</Field.Label>
+                                    <Input 
+                                        type="username"
+                                        value={username}
+                                        onChange={({ target }) => setUsername(target.value)} />
+                                </Field.Root>
+
+                            <Field.Root>
+                                <Field.Label>Password</Field.Label>
+                                    <PasswordInput 
+                                        type="password"
+                                        value={password}
+                                        onChange={({ target }) => setPassword(target.value)} />
+                            </Field.Root>
+
+                            <Button 
+                                type="submit"
+                                bg="#35544f"
+                            >
+                                Submit</Button>
+                            </Stack>
+                        </form>
+                    </Box>
                 </GridItem>
             </Grid>
         </Flex>
