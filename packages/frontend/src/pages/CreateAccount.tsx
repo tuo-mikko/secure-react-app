2fe4f5bddbc5a281721ac4ddb919cf42c527e95b
olivena <olivia.takkinen@gmail.com>
2025-04-23 17:15:27 +0300

    users are added to database

--- packages/frontend/src/pages/CreateAccount.tsx
+++ packages/frontend/src/pages/CreateAccount.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
 import { PasswordInput, PasswordStrengthMeter } from "../components/ui/password-input"
 import { PasswordStrengthService } from '../services/PasswordStrengthService'
-import { useForm } from "react-hook-form"
+import { useForm, SubmitHandler } from "react-hook-form"
 import axios from 'axios'
-import loginService from '../services/login'
 import { 
     Field,
     Flex,
@@ -17,33 +16,36 @@ import {
 const CreateAccount = () =>  {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
-    const [name, setName] = React.useState('');
-    const [user, setUser] = React.useState(null);
     const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
     const [passwordStrength, setPasswordStrength] = React.useState<number>(0);
     const service = new PasswordStrengthService();
+    const baseUrl = 'http://localhost:3001/api/users'
+    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<FormData>();
 
-    //TODO: Edit to handle registering new account
-    const handleLogin = async (event: React.FormEvent<HTMLElement>) => {
-        event.preventDefault()    
-        console.log('logging in with', username, password)  
-        console.log('password strength', passwordStrength)
+    type FormData = {
+        username: string;
+        name: string;
+        password: string;
+    };
 
-        try {
-            const user = await loginService.login({
-                username, password,
-            });
-
-            setUser(user)
-            setUsername('')
-            setPassword('')
-        } catch (exception) {
-            setErrorMessage('wrong credentials')
-            setTimeout(() => {
-                setErrorMessage(null)
-            }, 5000)
-        }
-    }
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        axios.post(baseUrl, {
+            username: data.username,
+            name: data.name,
+            password: data.password,
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    };
 
     return (
         <Flex
@@ -66,38 +68,33 @@ const CreateAccount = () =>  {
                     margin="2"
                     width="xl"
                     >
-                    <form onSubmit={handleLogin}>
-                    <Stack gap="4" align="flex-start">
-                        <Field.Root>
-                            <Field.Label>Username</Field.Label>
-                            <Input 
-                                type="username"
-                                value={username}
-                                onChange={({ target }) => setUsername(target.value)} />
-                        </Field.Root>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Stack gap="4" align="flex-start">
+                            <Field.Root>
+                                <Field.Label>Username</Field.Label>
+                                <Input 
+                                    {...register('username', { required: 'Username is required'})} />
+                            </Field.Root>
 
-                        <Field.Root>
-                            <Field.Label>Name</Field.Label>
-                            <Input 
-                                type="name"
-                                value={name}
-                                onChange={({ target }) => setUsername(target.value)} />
-                        </Field.Root>
+                            <Field.Root>
+                                <Field.Label>Name</Field.Label>
+                                <Input 
+                                    {...register('name', { required: 'Name is required'})} />
+                            </Field.Root>
 
-                        <Field.Root maxWidth="xl">
-                            <Stack>
-                                <Field.Label>Password</Field.Label>
-                                <Text 
-                                    fontStyle="italic"
-                                    textStyle="sm">Minimum length 8 characters, please use numbers and capitalized letters</Text>
-                                <PasswordInput 
-                                    type="password"
-                                    value={password}
-                                    onChange={({ target }) => {
-                                        setPassword(target.value);
-                                        setPasswordStrength(service.checkPasswordStrength(target.value));
-                                    }}
-                                        />
+                            <Field.Root maxWidth="xl">
+                                <Stack>
+                                    <Field.Label>Password</Field.Label>
+                                    <Text 
+                                        fontStyle="italic"
+                                        textStyle="sm">Minimum length 8 characters, please use numbers and capitalized letters</Text>
+                                    <PasswordInput 
+                                        {...register('password', { required: 'Password is required'})}
+                                        onChange={({ target }) => {
+                                            setPassword(target.value);
+                                            setPasswordStrength(service.checkPasswordStrength(target.value));
+                                        }}
+                                    />
                                  <PasswordStrengthMeter value={passwordStrength}/>
                             </Stack>
                         </Field.Root>
